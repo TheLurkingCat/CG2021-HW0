ece23e55606097a2d1972539e724e009e8b96866
P.T.Liu <meow.cs07@nctu.edu.tw>
2021-09-19 21:35:12 +0800

    Add release script

---
+++ script/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+zip -r9 HW0.zip \
+extern/glad \
+extern/glfw/CMake \
+extern/glfw/deps/mingw \
+extern/glfw/deps/vs2008 \
+extern/glfw/include \
+extern/glfw/src \
+extern/glfw/LICENSE.md \
+extern/glfw/CMakeLists.txt \
+cmake \
+include \
+src \
+.clang-format \
+CMakeLists.txt \
+README.md
+
